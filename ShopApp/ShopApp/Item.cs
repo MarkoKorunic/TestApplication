033f06845ba2a42f93aafc724b1c2a2e782838e1
MarkoKorunic <marko.korunic29@gmail.com>
2020-10-20 15:50:38 +0200

    ClassSetup1

---
+++ ShopApp/ShopApp/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp
+{
+    public class Item
+    {
+        public int Price { get; set; }
+        public string ItemName { get; set; }
+        public ItemType ItemType { get; set; }
+
+    }
+}
+
+public enum ItemType
+{
+    Weapon = 1,
+    Apparel = 2,
+    Potion = 3,
+    Material = 4,
+    Trinket = 5,
+    Book = 6,
+    Food = 7,
+    Drink = 8,
+}
--- ShopApp/ShopApp/Program.cs
+++ ShopApp/ShopApp/Program.cs
@@ -10,7 +10,12 @@ namespace ShopApp
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Welcome to Aurora's Realm Shop.");
+            Console.ReadLine();
+            Console.WriteLine("We have a selection of goods available to you at best prices in all Waterdeep.");
+            Console.ReadLine();
+            Console.WriteLine("Feel free to browse.");
+            StartBrowseScreen browseScreen = new StartBrowseScreen();
 
         }
     }
